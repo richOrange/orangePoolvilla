e15a22179639a5ae3447d7040ded9e23851b73af
tqfa2 <tqfa2@DESKTOP-KMQKC39>
2022-05-16 13:07:28 +0900

    [나재선] MyPageOneController.java 수정

--- src/main/java/controller/MyPageOneController.java
+++ src/main/java/controller/MyPageOneController.java
@@ -1,14 +1,12 @@
 package controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import dao.CustomerDao;
 import vo.Customer;
@@ -17,15 +15,12 @@ import vo.Customer;
 public class MyPageOneController extends HttpServlet {
 	private CustomerDao customerDao;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//session 값 요청
-				HttpSession session = request.getSession();
-			    String sessionCustomerId = (String)session.getAttribute("sessionCustomerId");
-			    //모델 호출
-			    customerDao = new CustomerDao();
-			    Customer customer = customerDao.selectMyPage(sessionCustomerId);
-			    ArrayList<Customer> list = customerDao.myPageCustomer();
-			    request.setAttribute("list", list);
-			    request.getRequestDispatcher("/WEB-INF/view/myPageOne.jsp").forward(request, response);
+		
+		String customerId = request.getParameter("customerId");
+		
+		Customer customer = customerDao.myPageCustomer(customerId);
+		
+		request.setAttribute("customer", customer);
 	}
 
 
