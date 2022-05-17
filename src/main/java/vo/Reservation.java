8957214590c74f49472d86bed1b564e18e7d9516
FryCandy <FryCandy@DESKTOP-SC45J2U>
2022-05-17 10:52:01 +0900

    [김득환] vo.Reservation 오타수정

--- src/main/java/vo/Reservation.java
+++ src/main/java/vo/Reservation.java
@@ -3,17 +3,24 @@ package vo;
 public class Reservation {
 	private int reservationNo;
 	private String customerId;
-	private String pvNo;
+	private int pvNo;
 	private String reservationBeginDate;
 	private String reservationLastDate;
 	private String reservationStatus;
 	private String createDate;
 	private String updateDate;
-	public Reservation() {
-		super();
-		// TODO Auto-generated constructor stub
+	
+	public Reservation() {}
+
+	@Override
+	public String toString() {
+		return "Reservation [reservationNo=" + reservationNo + ", customerId=" + customerId + ", pvNo=" + pvNo
+				+ ", reservationBeginDate=" + reservationBeginDate + ", reservationLastDate=" + reservationLastDate
+				+ ", reservationStatus=" + reservationStatus + ", createDate=" + createDate + ", updateDate="
+				+ updateDate + "]";
 	}
-	public Reservation(int reservationNo, String customerId, String pvNo, String reservationBeginDate,
+
+	public Reservation(int reservationNo, String customerId, int pvNo, String reservationBeginDate,
 			String reservationLastDate, String reservationStatus, String createDate, String updateDate) {
 		super();
 		this.reservationNo = reservationNo;
@@ -25,60 +32,72 @@ public class Reservation {
 		this.createDate = createDate;
 		this.updateDate = updateDate;
 	}
-	@Override
-	public String toString() {
-		return "Reservation [reservationNo=" + reservationNo + ", customerId=" + customerId + ", pvNo=" + pvNo
-				+ ", reservationBeginDate=" + reservationBeginDate + ", reservationLastDate=" + reservationLastDate
-				+ ", reservationStatus=" + reservationStatus + ", createDate=" + createDate + ", updateDate="
-				+ updateDate + "]";
-	}
+
 	public int getReservationNo() {
 		return reservationNo;
 	}
+
 	public void setReservationNo(int reservationNo) {
 		this.reservationNo = reservationNo;
 	}
+
 	public String getCustomerId() {
 		return customerId;
 	}
+
 	public void setCustomerId(String customerId) {
 		this.customerId = customerId;
 	}
-	public String getPvNo() {
+
+	public int getPvNo() {
 		return pvNo;
 	}
-	public void setPvNo(String pvNo) {
+
+	public void setPvNo(int pvNo) {
 		this.pvNo = pvNo;
 	}
+
 	public String getReservationBeginDate() {
 		return reservationBeginDate;
 	}
+
 	public void setReservationBeginDate(String reservationBeginDate) {
 		this.reservationBeginDate = reservationBeginDate;
 	}
+
 	public String getReservationLastDate() {
 		return reservationLastDate;
 	}
+
 	public void setReservationLastDate(String reservationLastDate) {
 		this.reservationLastDate = reservationLastDate;
 	}
+
 	public String getReservationStatus() {
 		return reservationStatus;
 	}
+
 	public void setReservationStatus(String reservationStatus) {
 		this.reservationStatus = reservationStatus;
 	}
+
 	public String getCreateDate() {
 		return createDate;
 	}
+
 	public void setCreateDate(String createDate) {
 		this.createDate = createDate;
 	}
+
 	public String getUpdateDate() {
 		return updateDate;
 	}
+
 	public void setUpdateDate(String updateDate) {
 		this.updateDate = updateDate;
 	}
+	
+	
+	
 	
 }
