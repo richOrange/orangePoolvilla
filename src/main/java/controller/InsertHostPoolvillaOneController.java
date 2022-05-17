c80841a484621b3c9a5ba2b47f3f1df139e28611
eicosaa <fomalhaut612@naver.com>
2022-05-17 10:48:15 +0900

    [김민서, 서경록] 풀빌라 상품 등록 기능 구현 중
    insertHostPoolvillaOne.jsp 추가,
    InsertHostPoolvillaOneController 추가

---
+++ src/main/java/controller/InsertHostPoolvillaOneController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/host/insertHostPoolvillaOneController")
+public class InsertHostPoolvillaOneController extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//jsp 호출
+		request.getRequestDispatcher("/WEB-INF/view/insertHostPoolvillaOne.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
